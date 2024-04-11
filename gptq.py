296c32aa59d5dadef35541556ff26fc30ba7b120
AmirhosseinBZRG <118589755+AmirhosseinBZRG@users.noreply.github.com>
2024-04-11 16:28:19 +0330

    Update gptq.py

--- gptq.py
+++ gptq.py
@@ -4,87 +4,61 @@ import numpy as np
 import torch
 import torch.nn as nn
 import transformers
-
 from quant import *
-
-
 DEBUG = False
-
 torch.backends.cuda.matmul.allow_tf32 = False
 torch.backends.cudnn.allow_tf32 = False
 def dct(src, dim=-1, norm='ortho'):
     # type: (torch.tensor, int, str) -> torch.tensor
-
     x = src.clone()
     N = x.shape[dim]
-
     x = x.transpose(dim, -1)
     x_shape = x.shape
     x = x.contiguous().view(-1, N)
-
     v = torch.empty_like(x, device=x.device)
     v[..., :(N - 1) // 2 + 1] = x[..., ::2]
-
     if N % 2:  # odd length
         v[..., (N - 1) // 2 + 1:] = x.flip(-1)[..., 1::2]
     else:  # even length
         v[..., (N - 1) // 2 + 1:] = x.flip(-1)[..., ::2]
-
     V = torch.fft.fft(v, dim=-1)
-
     k = torch.arange(N, device=x.device)
     V = 2 * V * torch.exp(-1j * np.pi * k / (2 * N))
-
     if norm == 'ortho':
         V[..., 0] *= math.sqrt(1/(4*N))
         V[..., 1:] *= math.sqrt(1/(2*N))
-
     V = V.real
     V = V.view(*x_shape).transpose(-1, dim)
-
     return V
-
 def idct(src, dim=-1, norm='ortho'):
     # type: (torch.tensor, int, str) -> torch.tensor
-
     X = src.clone()
     N = X.shape[dim]
-
     X = X.transpose(dim, -1)
     X_shape = X.shape
     X = X.contiguous().view(-1, N)
-
     if norm == 'ortho':
         X[..., 0] *= 1 / math.sqrt(2)
         X *= N*math.sqrt((2 / N))
     else:
         raise Exception("idct with norm=None is buggy A.F")
-
     k = torch.arange(N, device=X.device)
-
     X = X * torch.exp(1j * np.pi * k / (2 * N))
     X = torch.fft.ifft(X, dim=-1).real
     v = torch.empty_like(X, device=X.device)
-
     v[..., ::2] = X[..., :(N - 1) // 2 + 1]
     v[..., 1::2] = X[..., (N - 1) // 2 + 1:].flip(-1)
-
     v = v.view(*X_shape).transpose(-1, dim)
     return v
-
 def dct_2d(x, norm='ortho'):
-
     X1 = dct(x, norm=norm)
     X2 = dct(X1.transpose(-1, -2), norm=norm)
     return X2.transpose(-1, -2)
-
 def idct_2d(X, norm='ortho'):
-
     x1 = idct(X, norm=norm)
     x2 = idct(x1.transpose(-1, -2), norm=norm)
     return x2.transpose(-1, -2)
 class GPTQ:
-
     def __init__(self, layer):
         self.layer = layer
         self.dev = self.layer.weight.device
@@ -97,7 +71,6 @@ class GPTQ:
         self.columns = W.shape[1]
         self.H = torch.zeros((self.columns, self.columns), device=self.dev)
         self.nsamples = 0
-
     def add_batch(self, inp, out):
         if DEBUG:
             self.inp1 = inp
@@ -125,7 +98,6 @@ class GPTQ:
         inp = math.sqrt(2 / self.nsamples) * inp.float()
         # self.H += 2 / self.nsamples * inp.matmul(inp.t())
         self.H += inp.matmul(inp.t())
-
     def fasterquant(
         self, blocksize=128, percdamp=.01, groupsize=-1, actorder=False, static_groups=False,
         dct_mode = 0
@@ -136,18 +108,14 @@ class GPTQ:
         if isinstance(self.layer, transformers.Conv1D):
             W = W.t()
         W = W.float()
-
         tick = time.time()
-
         if not self.quantizer.ready():
             self.quantizer.find_params(W, weight=True)
-
         H = self.H
         del self.H
         dead = torch.diag(H) == 0
         H[dead, dead] = 1
         W[:, dead] = 0
-
         if static_groups:
             import copy
             groups = []
@@ -155,16 +123,13 @@ class GPTQ:
                 quantizer = copy.deepcopy(self.quantizer)
                 quantizer.find_params(W[:, i:(i + groupsize)], weight=True)
                 groups.append(quantizer)
-
         if actorder:
             perm = torch.argsort(torch.diag(H), descending=True)
             W = W[:, perm]
             H = H[perm][:, perm]
             invperm = torch.argsort(perm)
-
         Losses = torch.zeros_like(W)
         Q = torch.zeros_like(W)
-
         damp = percdamp * torch.mean(torch.diag(H))
         diag = torch.arange(self.columns, device=self.dev)
         H[diag, diag] += damp
@@ -172,95 +137,72 @@ class GPTQ:
         H = torch.cholesky_inverse(H)
         H = torch.linalg.cholesky(H, upper=True)
         Hinv = H
-
         for i1 in range(0, self.columns, blocksize):
             i2 = min(i1 + blocksize, self.columns)
             count = i2 - i1
-
             W1 = W[:, i1:i2].clone()
             Q1 = torch.zeros_like(W1)
             Err1 = torch.zeros_like(W1)
             Losses1 = torch.zeros_like(W1)
             Hinv1 = Hinv[i1:i2, i1:i2]
+            for i in range(count):
+                w = W1[:, i]
+                d = Hinv1[i, i]
 
-            for i in range(count):  # loop over each column of W1
-                w = W1[:, i]  # get the current column
-                d = Hinv1[i, i]  # get the corresponding diagonal element of Hinv1
+                if dct_mode == 1: # if DCT mode is enabled
 
-                if dct_mode == 1:  # if DCT mode is enabled
-
-                    if groupsize != -1:  # if groupsize is not -1
+                
+                    if groupsize != -1: # if groupsize is not -1
                         if not static_groups:  # if groups are not static
                             if (i1 + i) % groupsize == 0:  # if the current column is the first column of a group
-                                W_group = torch.split(W[:, (i1 + i):(i1 + i + groupsize)], groupsize, dim=1)  # split W into groups
-                                dct_W_group = torch.stack([dct(column, norm='ortho') for column in W_group], dim=1)  # apply DCT to each group
-                                self.quantizer.find_params(dct_W_group, weight=True)  # find quantization parameters for each group
-                                dct_w = dct_W_group[:, i]  # get the DCT coefficients for the current column
-                                quantized_dct_w = quantize(
-                                    dct_w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
-                                ).flatten()  # quantize the DCT coefficients
-                                q = idct(quantized_dct_w.unsqueeze(0), norm='ortho').squeeze(0)  # apply inverse DCT to get the quantized column
-                            else:  # if the current column is not the first column of a group
-                                dct_w = dct(w.unsqueeze(0), norm='ortho').squeeze(0)  # apply DCT to the current column
-                                quantized_dct_w = quantize(
-                                    dct_w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
-                                ).flatten()  # quantize the DCT coefficients
-                                q = idct(quantized_dct_w.unsqueeze(0), norm='ortho').squeeze(0)  # apply inverse DCT to get the quantized column
-                        else:  # if groups are static
-                            idx = i1 + i  # compute the index of the current column in the original weight matrix
-                            if actorder:  # if activation ordering is enabled
-                                idx = perm[idx]  # apply the permutation to the index
-                                self.quantizer = groups[idx // groupsize]  # get the quantizer for the current group
-                            dct_w = dct(w.unsqueeze(0), norm='ortho').squeeze(0)  # apply DCT to the current column
-                            quantized_dct_w = quantize(
-                                dct_w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
-                            ).flatten()  # quantize the DCT coefficients
-                            q = idct(quantized_dct_w.unsqueeze(0), norm='ortho').squeeze(0)  # apply inverse DCT to get the quantized column
-                    else:  # if groupsize is -1
-                        idx = i1 + i  # compute the index of the current column in the original weight matrix
-                        if actorder:  # if activation ordering is enabled
-                            idx = perm[idx]  # apply the permutation to the index
-                            self.quantizer = groups[idx // groupsize]  # get the quantizer for the current group
-                        dct_w = dct(w.unsqueeze(0), norm='ortho').squeeze(0)  # apply DCT to the current column
-                        quantized_dct_w = quantize(
-                            dct_w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
-                        ).flatten()  # quantize the DCT coefficients
-                        q = idct(quantized_dct_w.unsqueeze(0), norm='ortho').squeeze(0)  # apply inverse DCT to get the quantized column
-                else: 
-                    q = quantize(w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq).flatten()
-                    
+                                W_group = torch.split(W[:, (i1 + i):(i1 + i + groupsize)], groupsize, dim=1) # split W into groups
+                                dct_W_group = torch.stack([dct(column, norm='ortho') for column in W_group], dim=1) # apply DCT to each group
+                                self.quantizer.find_params(dct_W_group, weight=True) # find quantization parameters for each group
+                    else:
+                        idx = i1 + i 
+                    if actorder:
+                        idx = perm[idx]
+                        self.quantizer = groups[idx // groupsize]
+
+                
+
+                    dct_w = dct(w.unsqueeze(0), norm='ortho').squeeze(0)
+                    quantized_dct_w = quantize(
+                        dct_w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
+                    ).flatten()
+                    q = idct(quantized_dct_w.unsqueeze(0), norm='ortho').squeeze(0)
+                else:
+                    q = quantize(
+                        w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
+                    ).flatten()
+             
+
                 q = q.unsqueeze(0).repeat(count, 1)
-                Q1[:, i] = q
+                q = q.transpose(0, 1)
+                Q1[:, i * count:(i + 1) * count] = q
                 Losses1[:, i] = (w - q) ** 2 / d ** 2
 
                 err1 = (w - q) / d
                 W1[:, i:] -= err1.unsqueeze(1).matmul(Hinv1[i, i:].unsqueeze(0))
                 Err1[:, i] = err1
-
             Q[:, i1:i2] = Q1
             Losses[:, i1:i2] = Losses1 / 2
-
             W[:, i2:] -= Err1.matmul(Hinv[i1:i2, i2:])
-
             if DEBUG:
                 self.layer.weight.data[:, :i2] = Q[:, :i2]
                 self.layer.weight.data[:, i2:] = W[:, i2:]
                 print(torch.sum((self.layer(self.inp1) - self.out1) ** 2))
                 print(torch.sum(Losses))
-
         torch.cuda.synchronize()
         print('time %.2f' % (time.time() - tick))
         print('error', torch.sum(Losses).item())
-
         if actorder:
             Q = Q[:, invperm]
-
         if isinstance(self.layer, transformers.Conv1D):
             Q = Q.t()
         self.layer.weight.data = Q.reshape(self.layer.weight.shape).to(self.layer.weight.data.dtype)
         if DEBUG:
             print(torch.sum((self.layer(self.inp1) - self.out1) ** 2))
-
     def free(self):
         if DEBUG:
             self.inp1 = None
